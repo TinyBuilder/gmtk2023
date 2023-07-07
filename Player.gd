0d16acdad61416764ef58c1f96ce53235c86dc6c
Alex (Ikwaylx) <alexanderjwork@gmail.com>
2023-07-07 22:22:11 +0100

    Project organization, input mapping, player movement
    Added in script files for main scenes
    
    Added input mapping - WASD / arrow keys and spacebar for shooting
    
    Added 4 directional discrete movement

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends Area2D
-
+# Set variables
+@export var speed = 400
+var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +10,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# TODO: Player movement - Four directional movement no diagonals
+	velocity = Vector2.ZERO
+	# Input mapping
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	elif Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	elif Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	elif Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	# Update player position
+	position += velocity * delta
+
