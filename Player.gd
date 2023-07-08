3506d24175d2075110b25196f84f787ab8be2adf
Alex (Ikwaylx) <alexanderjwork@gmail.com>
2023-07-08 14:27:37 +0100

    BAH
    I give - I've changed the player node to a characterobject but it is not playing well with input. Collision worked but 4 directional input machine broke

--- Player.gd
+++ Player.gd
@@ -1,12 +1,8 @@
-extends RigidBody2D
+extends CharacterBody2D
 # Set variables and signals
-@export var speed = 400
-var velocity
+var speed = 400
 var screen_size
 signal hit
-
-
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,11 +10,9 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func get_input():
 	velocity = Vector2.ZERO
-	# Input mapping
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_up"):		
 		velocity.y -= 1
 	elif Input.is_action_pressed("move_down"):
 		velocity.y += 1
@@ -27,28 +21,42 @@ func _process(delta):
 	elif Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	
-	# Updating player speed, plays animation if moving
-	if velocity.length() > 0:
-		velocity = velocity * speed
-		$AnimatedSprite2D.play()
-	
-	# If not moving - play an idle animation
-	else:
-		$AnimatedSprite2D.play("Idle_animation")
-	
-	# Handle which animation to play based on current movement
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "move_side"
-		$AnimatedSprite2D.flip_h = velocity.x > 0
-	
-	if velocity.y < 0:
-		$AnimatedSprite2D.animation = "move_up"
-	
-	if velocity.y > 0:
-		$AnimatedSprite2D.animation = "move_down"
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
+	# Input mapping
+#	if Input.is_action_pressed("move_up"):
+#		velocity.y -= 1
+#	elif Input.is_action_pressed("move_down"):
+#		velocity.y += 1
+#	elif Input.is_action_pressed("move_left"):
+#		velocity.x -= 1
+#	elif Input.is_action_pressed("move_right"):
+#		velocity.x += 1
+#
+#	# Updating player speed, plays animation if moving
+#	if velocity.length() > 0:
+#		$AnimatedSprite2D.play()
+#
+#	# If not moving - play an idle animation
+#	else:
+#		$AnimatedSprite2D.play("idle")
+#
+#	# Handle which animation to play based on current movement
+#	if velocity.x != 0:
+#		$AnimatedSprite2D.animation = "move_left"
+#		$AnimatedSprite2D.flip_h = velocity.x > 0
+#
+#	if velocity.y < 0:
+#		$AnimatedSprite2D.animation = "move_up"
+#
+#	if velocity.y > 0:
+#		$AnimatedSprite2D.animation = "move_down"
 
 	# Update player position
-	position += velocity * delta
+
+
 	
 
 	
